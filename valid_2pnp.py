b9f2468e344d82e7b39ad127ffc79b39d1d55f9c
X Hu <xh297@cam.ac.uk>
2019-08-29 21:30:43 +0800

    .

--- valid_2pnp.py
+++ valid_2pnp.py
@@ -131,6 +131,7 @@ def valid(datacfg, cfgfile, weightfile, outfile, test=True):
         data1 = Variable(data[0], volatile=True)
         data2 = Variable(data[1], volatile=True)
         t2 = time.time()
+        print(data.shape)
         print(data1.shape)
         
         # Forward pass
